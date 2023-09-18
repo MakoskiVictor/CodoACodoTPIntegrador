7eed40de53af64c581c2c202b6c68c7a976cd1cd
MakoskiVictor <makoski.ed@gmail.com>
2023-09-18 20:10:15 -0300

    import navbarmodule through

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './modules/core';
+import { NavBarComponent } from './modules/shared/components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
