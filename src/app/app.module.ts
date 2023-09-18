4536d56e57ea77c4574a62c41490c1e3e3bdb3d2
MakoskiVictor <makoski.ed@gmail.com>
2023-09-18 20:11:51 -0300

    update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './modules/core';
-import { NavBarComponent } from './modules/shared/components/nav-bar/nav-bar.component';
+import { NavBarComponent } from './modules';
+
 
 @NgModule({
   declarations: [
