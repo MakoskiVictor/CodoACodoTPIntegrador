dcbb7ef7c7dc2de413e8cf0a7b8acd66ff4cfeb8
MakoskiVictor <makoski.ed@gmail.com>
2023-09-18 21:15:01 -0300

    add HiperlinkComponent into declarations

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components';
+import { HiperlinkComponent } from './components/hiperlink/hiperlink.component';
 
 
 
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    HiperlinkComponent
   ],
   imports: [
     CommonModule
